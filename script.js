a0d8fe929dd499505cef4b1981174d1efb7b0dd5
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-03-27 20:40:31 -0600

    Make grid cells square

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //create a 16x16 grid of square divs
 const container = document.getElementById('container');
-for (let i = 0; i < 256; i++) {
+/*
+i needs to be 272 (256 + 16) because each 17th div is used to 
+line-break instead of serving as an actual square in the grid
+*/
+for (let i = 0; i < 272; i++) {
     let square = document.createElement('div');
     square.className = 'square';
-    square.textContent = "o";
+    square.textContent = "";
     container.appendChild(square);
 }
