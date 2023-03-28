5186f87c3d1b2cede10c12c57d9164ab14afa5da
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-03-27 19:40:08 -0600

    Make a 16x16 grid w/ flexbox

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //create a 16x16 grid of square divs
-const container = document.querySelector('container');
-for (let i = 0; i < 16; i++) {
-    const square = document.createElement('div');
+const container = document.getElementById('container');
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement('div');
+    square.className = 'square';
+    square.textContent = "o";
     container.appendChild(square);
 }
