1974f83f6a1da8a645703cf0f5703c336cb902a4
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-04-02 16:41:21 -0600

    Add mouseover effect

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //create a 16x16 grid of square divs
 const container = document.getElementById('container');
 /*
-i needs to be 272 (256 + 16) because each 17th div is used to 
-line-break instead of serving as an actual square in the grid
+i needs to be 272 (256 + 16) because each the 1st, 18th, 35th, etc.
+divs are used to line-break instead of serving as an actual square 
+in the grid
 */
 for (let i = 0; i < 272; i++) {
     let square = document.createElement('div');
     square.className = 'square';
     square.textContent = "";
+    square.addEventListener('mouseover', () => {
+        square.style.background = 'black';
+    })
     container.appendChild(square);
 }
