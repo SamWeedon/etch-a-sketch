723f155ffb4ae689e3b636ccd1d8170e38fcb2e0
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-04-03 21:17:48 -0600

    Rework js and css to make it more elegant

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-//create a 16x16 grid of square divs
 const container = document.getElementById('container');
-/*
-i needs to be 272 (256 + 16) because each the 1st, 18th, 35th, etc.
-divs are used to line-break instead of serving as an actual square 
-in the grid
-*/
-for (let i = 0; i < 272; i++) {
+
+for (let i = 0; i < 256; i++) {
     let square = document.createElement('div');
     square.className = 'square';
-    square.textContent = "";
     square.addEventListener('mouseover', () => {
         square.style.background = 'black';
     })
