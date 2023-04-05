ec9a863b07c05bd2e1fe5a7d81a1b9ed737936bb
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-04-04 19:57:06 -0600

    Move line break logic from css to js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const container = document.getElementById('container');
 
-for (let i = 0; i < 256; i++) {
+for (let i = 1; i < 257; i++) {
     let square = document.createElement('div');
     square.className = 'square';
     square.addEventListener('mouseover', () => {
         square.style.background = 'black';
     })
     container.appendChild(square);
+    if (i % 16 === 0) {
+        let brk = document.createElement('div');
+        brk.className = 'brk';
+        container.appendChild(brk);
+    }
 }
